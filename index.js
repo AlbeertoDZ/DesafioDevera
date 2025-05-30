b2b2f4747f09dfc3cbb290e70d65e3c5bfc77970
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-30 12:15:47 +0200

    Configura servidor para despliegue en Render

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const path = require("path");
+const port = process.env.PORT || 3001;
+
+require('dotenv').config();// Cargar variables de entorno
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
+app.use(express.static("public")); // Middleware para servir archivos estáticos de front
+
+// Servir archivos generados por Vite en /client/dist
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
