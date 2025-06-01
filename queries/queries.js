90f314e0d064bdd46b3715ddccd3380aa197c202
AlbeertoDZ <albertodiezalcobendas@gmail.com>
2025-06-01 16:59:27 +0200

    endpoints users

---
+++ queries/queries.js
@@ -0,0 +1,10 @@
+const queries = {
+    //USERS
+
+    getAllUsers: `SELECT * FROM users;`,
+    getUserById: `SELECT * FROM users WHERE id = $1`,
+    getUserByEmail: `SELECT * FROM users WHERE email = $1`,
+    createUser: `INSERT INTO users (name, lastname, email, password, created_at, id_empresa)
+                 VALUES ($1, $2, $3, $4, NOW(), $5)
+                 RETURNING *`,
+}
