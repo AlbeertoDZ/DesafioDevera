f41fcb7e045707be047a8f01f47862f1da4b57d2
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-06-02 12:28:11 +0200

    corecciones app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
-// import Login from './components/Login/Login'
-// import './App.css'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Login />
-//     </div>
-//   )
-// }
-
-// export default App
-
+import Login from './components/Login/Login'
+import './App.css'
 import Home from "./components/Home/Home";
 
+
 function App() {
-  return <Home />;
+  return (
+    <div className="App">
+      <Login />
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
