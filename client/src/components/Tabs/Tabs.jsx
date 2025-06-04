45578631d866f595910796639e356e2b1865823b
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-06-04 16:58:10 +0200

    add/ruta files

--- client/src/components/Tabs/Tabs.jsx
+++ client/src/components/Tabs/Tabs.jsx
@@ -1,9 +1,10 @@
-// src/components/Tabs/Tabs.jsx
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Tabs.scss';
 
 const Tabs = ({ onSelect }) => {
   const [activeTab, setActiveTab] = useState('Mis productos');
+  const navigate = useNavigate();
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -14,21 +15,22 @@ const Tabs = ({ onSelect }) => {
     <div className="tabs-container">
       <button
         className={activeTab === 'Mis productos' ? 'tab active' : 'tab'}
-        onClick={() => handleTabClick('Mis productos')}
+        onClick={() => {
+          handleTabClick('Mis productos');
+          navigate('/dashboard');
+        }}
       >
         Mis productos
       </button>
+
       <button
-        className={activeTab === 'Archivos' ? 'tab' : 'tab disabled'}
-        disabled
+        className={activeTab === 'Archivos' ? 'tab active' : 'tab'}
+        onClick={() => {
+          handleTabClick('Archivos');
+          navigate('/files');
+        }}
       >
         Archivos
-      </button>
-      <button
-        className={activeTab === 'Información general' ? 'tab' : 'tab disabled'}
-        disabled
-      >
-        Información general
       </button>
     </div>
   );
