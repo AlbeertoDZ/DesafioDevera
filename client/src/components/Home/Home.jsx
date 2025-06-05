dfb107f6681fa31c13f301a6317097efbc7a089f
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-06-05 12:17:26 +0200

    deleted pagination fromdashboard + modification on search component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => (
     <Tabs />
     <SearchBar />
     <ProductTable />
-    <Pagination />
   </div>
 );
 
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -7,16 +7,6 @@ const SearchBar = () => {
 
   return (
     <div className="top-bar">
-      <div className="top-bar-left">
-        <label htmlFor="entries">Mostrando</label>
-        <select id="entries" name="entries">
-          <option value="10">10</option>
-          <option value="25">25</option>
-          <option value="50">50</option>
-        </select>
-        <span>productos</span>
-      </div>
-
       <div className="top-bar-right">
         <input
           type="text"
